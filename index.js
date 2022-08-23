5fdd94a1701f54cfd76695e420ae75ba976ee823
Yireth Mendoza <yiyimendoza98@gmail.com>
2022-08-23 15:36:41 -0500

    cambio a airam

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let nombre = "alan"
+let nombre = "airam"
 
 let nombreUsuario = prompt ("Escribe tu nombre")
 if (nombre=== nombreUsuario) {
-    alert ("Hola amix, te tqm");
+    alert ("Hola amix, me la pelas jeje");
 } else {
-    alert ("Regresa cuando seas Alan")
+    alert ("Regresa cuando seas airam")
 }
+
