722f54684e2c8b6070163dd3017308a0c31b99cf
Yireth Mendoza <yiyimendoza98@gmail.com>
2022-08-23 18:32:26 -0500

    cambio a cristina

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let nombre = "airam"
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); 
+  };
+let nombre = "cristina"
 
 let nombreUsuario = prompt ("Escribe tu nombre")
-if (nombre=== nombreUsuario) {
-    alert ("Hola amix, me la pelas jeje");
+if (removeAccents(nombreUsuario.toLowerCase()) === nombre) {
+    alert ("Animo abue! te quiero mucho:)");
 } else {
-    alert ("Regresa cuando seas airam")
+    alert ("Regresa cuando seas Cristina")
 }
-
