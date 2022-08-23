dc2f20262823ec28bbc4ce457d7ae52cfc936d8e
Yireth Mendoza <yiyimendoza98@gmail.com>
2022-08-23 13:26:47 -0500

    hice un js

---
+++ index.js
@@ -0,0 +1,8 @@
+let nombre = "alan"
+
+let nombreUsuario = prompt ("Escribe tu nombre")
+if (nombre=== nombreUsuario) {
+    alert ("Hola amix, te tqm");
+} else {
+    alert ("Regresa cuando seas Alan")
+}
